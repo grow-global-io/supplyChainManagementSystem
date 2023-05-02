53428f665754436f25c79a407e8eb69d0b58a9f8
Tathagat Saha <99318598+tatz210588@users.noreply.github.com>
2023-05-02 22:07:21 +0200

    comm

---
+++ client/src/components/Theme.js
@@ -0,0 +1,15 @@
+import createTheme from "@material-ui/core/styles/createTheme";
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#1a237e",
+      },
+      secondary: {
+        main: "#1a237e",
+      },
+
+    },
+  });
+  
+  export default theme;
