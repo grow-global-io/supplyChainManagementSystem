e5827f19ea06487f00c8f727315540ee61866399
anshusinha872 <=>
2023-05-18 22:10:30 +0530

    financeRole(WIP)

--- FrontEnd/src/pages/FinanceManager/FinanceManager.js
+++ FrontEnd/src/pages/FinanceManager/FinanceManager.js
@@ -10,218 +10,237 @@ import {
   Table,
   Modal,
   Card,
+  Dropdown,
 } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
+import Select from "react-select";
 import SuppChain from "../../artifacts/contracts/SupplyChain.sol/SupplyChain.json";
 import { useState } from "react";
 import { ethers } from "ethers";
 import { getConfigByChain } from "../../assets/config";
-const navItem = [
-];
+import {
+  formatBigNumber,
+  getCollectionData,
+  getCollectionDataWithId,
+  saveData,
+} from "../../utils/fbutils";
+import { updateCollectionData } from "../../utils/fbutils";
+import { getStatus } from "../../assets/statusConfig";
+const navItem = [];
 export const FinanceManager = () => {
-  const [show, setShow] = useState(false);
-  const [role, setRole] = useState("");
-  const handleClose = () => setShow(false);
-  useEffect(()=>{
-    verifyRole()
-  },[])
+ // blockChainMasterData start
+ const [masterTableData, setMasterTableData] = useState([]);
+ //  blockChainMasterData end
+ const [role, setRole] = useState("");
+ const [save, setSave] = useState(false);
 
-  const verifyRole = async () => {
-    console.log("verifyRole");
-    await (window).ethereum.request({ method: "eth_requestAccounts", });
-      const provider = new ethers.providers.Web3Provider(window.ethereum) //create provider
-      const network = await provider.getNetwork()
-      const signer = provider.getSigner()
-      
-      const suppContract = new ethers.Contract(
-        getConfigByChain(network.chainId)[0].suppChainAddress,
-        SuppChain.abi,
-        signer
-      )
-      console.log("suppContract", suppContract);
-      const tx = await suppContract.getRole();
-      console.log("tx", tx);
-      setRole(tx);
-      console.log("role", role);
-  }
-  const handleShow = () => setShow(true);
-  if(role === "Sales Representative"){
-    return (
-      <Navbar pageTitle={"Delivery Hub"} navItems={navItem}>
-        <div>
-          <Container>
-          {
-            // <Button onClick={verifyRole} variant="primary">verifyRole</Button>{" "}
-          }
-            <Row>
-              <Card>
-                <Card.Body>
-                  <Col>
-                    <Tabs
-                      defaultActiveKey="createOrder"
-                      id="uncontrolled-tab-example"
-                      className="mb-3"
-                    >
-                      <Tab eventKey="createOrder" title="Create Order">
-                        <Form>
-                          <Form.Group
-                            className="mb-3 "
-                            controlId="exampleForm.ControlInput1"
-                          >
-                            <Form.Label className="text-left">
-                              Order Number
-                            </Form.Label>
-                            <Form.Control type="text" placeholder="SO" />
-                          </Form.Group>
-                          <Form.Group
-                            className="mb-3"
-                            controlId="exampleForm.ControlInput1"
-                          >
-                            <Form.Label>Customer Address</Form.Label>
-                            <Form.Control type="text" placeholder="" />
-                          </Form.Group>
-                          <Form.Group
-                            className="mb-3"
-                            controlId="exampleForm.ControlInput1"
-                          >
-                            <Form.Label>Status</Form.Label>
-                            <Form.Control type="text" placeholder="" />
-                          </Form.Group>
-                          <Form.Group
-                            className="mb-3"
-                            controlId="exampleForm.ControlInput1"
-                          >
-                            <Form.Label>Customer Email</Form.Label>
-                            <Form.Control type="email" placeholder="" />
-                          </Form.Group>
-                          <Form.Group
-                            className="mb-3"
-                            controlId="exampleForm.ControlInput1"
-                          >
-                            <Form.Label>Order Start Date</Form.Label>
-                            <Form.Control type="date" placeholder="" />
-                          </Form.Group>
-                          <Form.Group
-                            className="mb-3"
-                            controlId="exampleForm.ControlInput1"
-                          >
-                            <Form.Label>Customer Delivery Date</Form.Label>
-                            <Form.Control type="date" placeholder="" />
-                          </Form.Group>
-                          <Button variant="primary">Submit</Button>{" "}
-                        </Form>
-                      </Tab>
-                      <Tab eventKey="productList" title="Product List">
-                        <div className="d-flex justify-content-end">
-                          <Button
-                            onClick={handleShow}
-                            className="mb-3"
-                            variant="primary"
-                          >
-                            Create product
-                          </Button>{" "}
-                        </div>
-                        <Table striped bordered hover>
-                          <thead>
-                            <tr>
-                              <th>Product Code</th>
-                              <th>Product</th>
-                              <th>Quantity</th>
-                              <th>Unit Price</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr>
-                              <td>SO1</td>
-                              <td>test1</td>
-                              <td>1</td>
-                              <td>$10</td>
-                            </tr>
-                            <tr>
-                              <td>SO2</td>
-                              <td>test2</td>
-                              <td>1</td>
-                              <td>$10</td>
-                            </tr>
-                            <tr>
-                              <td>SO2</td>
-                              <td>test2</td>
-                              <td>1</td>
-                              <td>$10</td>
-                            </tr>
-                          </tbody>
-                        </Table>
-                        <Modal className="mt-5" show={show} onHide={handleClose}>
-                          <Modal.Header closeButton>
-                            <Modal.Title>Create Product</Modal.Title>
-                          </Modal.Header>
-                          <Modal.Body>
-                            <Form>
-                              <Form.Group
-                                className="mb-3"
-                                controlId="exampleForm.ControlInput1"
-                              >
-                                <Form.Label>Product Code</Form.Label>
-                                <Form.Control type="text" placeholder="" />
-                              </Form.Group>
-                              <Form.Group
-                                className="mb-3"
-                                controlId="exampleForm.ControlInput1"
-                              >
-                                <Form.Label>Product</Form.Label>
-                                <Form.Control type="text" placeholder="" />
-                              </Form.Group>
-                              <Form.Group
-                                className="mb-3"
-                                controlId="exampleForm.ControlInput1"
-                              >
-                                <Form.Label>Quantity</Form.Label>
-                                <Form.Control type="number" placeholder="" />
-                              </Form.Group>
-                              <Form.Group
-                                className="mb-3"
-                                controlId="exampleForm.ControlInput1"
-                              >
-                                <Form.Label>Unit Price</Form.Label>
-                                <Form.Control type="number" placeholder="" />
-                              </Form.Group>
-                            </Form>
-                          </Modal.Body>
-                          <Modal.Footer>
-                            <Button variant="secondary" onClick={handleClose}>
-                              Close
-                            </Button>
-                            <Button variant="primary" onClick={handleClose}>
-                              Save Product
-                            </Button>
-                          </Modal.Footer>
-                        </Modal>
-                      </Tab>
-                    </Tabs>
-                  </Col>
-                </Card.Body>
-              </Card>
-            </Row>
-          </Container>
-        </div>
-      </Navbar>
-    )
-  }
-  else{
-    return (
-      <Navbar pageTitle={"Delivery Hub"} navItems={navItem}>
-        <div>
-          <Container>
-            <Row>
-              <Card>
-                <Card.Body>
-                  <h1>You don't have permission</h1>
-                </Card.Body>
-              </Card>
-            </Row>
-          </Container>
-        </div>
-      </Navbar>
-    )
-  }
-}
+ const fetchCollectionData = async () => {};
+ useEffect(() => {
+   verifyRole();
+   fetchCollectionData();
+ }, [save]);
+ useEffect(() => {
+   console.log("masterTableData", masterTableData);
+ }, [masterTableData]);
+
+ const verifyRole = async () => {
+   console.log("verifyRole");
+   await window.ethereum.request({ method: "eth_requestAccounts" });
+   const provider = new ethers.providers.Web3Provider(window.ethereum); //create provider
+   const network = await provider.getNetwork();
+   const signer = provider.getSigner();
+
+   const suppContract = new ethers.Contract(
+     getConfigByChain(network.chainId)[0].suppChainAddress,
+     SuppChain.abi,
+     signer
+   );
+   console.log("suppContract", suppContract);
+   const tx = await suppContract.getRole();
+   setMasterTableData(await suppContract.getAllOrderDetails());
+   setRole(tx);
+ };
+
+ const updateBlockDataOrderStatus = async (soId, col, val) => {
+   // setLoading(true)
+   try {
+     console.log("soId", soId);
+     console.log("col", col);
+     console.log("val", val);
+     await window.ethereum.request({ method: "eth_requestAccounts" });
+     const provider = new ethers.providers.Web3Provider(window.ethereum); //create provider
+     const network = await provider.getNetwork();
+     const signer = provider.getSigner();
+
+     const suppContract = new ethers.Contract(
+       getConfigByChain(network.chainId)[0].suppChainAddress,
+       SuppChain.abi,
+       signer
+     );
+     console.log(soId);
+     const tx = await suppContract.update(soId, col, val);
+     console.log("tx", tx);
+     // toast('Role Assignment in progress !!', { icon: '👏' })
+   } catch (e) {
+     // toast.error('An error occured. Check console !!')
+     console.log(e);
+     // setLoading(false)
+   }
+ };
+ //
+ const [show, setShow] = useState(false);
+ const handleClose = () => setShow(false);
+ const [currentSoId, setCurrentSoId] = useState(0);
+ 
+ const handleShow = (item) => {
+   setShow(true);
+   setCurrentSoId(item[0]);
+   console.log("currentSoId", currentSoId);
+   console.log("item", item);
+ };
+ const [barCode, setBarCode] = useState("");
+ const [batchNumber, setBatchNumber] = useState("");
+ const [masterLabel, setMasterLabel] = useState("");
+ const handleChange = (e) => {
+   if (e.target.id === "barCode") {
+     setBarCode(e.target.value);
+   }
+   if (e.target.id === "batchNumber") {
+     setBatchNumber(e.target.value);
+   }
+   if (e.target.id === "masterLabel") {
+     setMasterLabel(e.target.value);
+   }
+ }
+ const handleSave = async () => {
+   handleClose();
+   console.log("handleSave");
+   console.log("currentSoId", currentSoId);
+   console.log("barCode", barCode);
+   console.log("batchNumber", batchNumber);
+   console.log("masterLabel", masterLabel);
+   await updateBlockDataOrderStatus(currentSoId, ["BarCode","BatchNo","Master Label","Status"], [barCode,batchNumber,masterLabel,"Ready for Customer Delivery"]);
+ }
+ if (true) {
+   return (
+     <Navbar pageTitle={"Delivery Hub"} navItems={navItem}>
+       <div>
+         <h1 style={{ color: "blue", fontSize: "32px", fontWeight: "normal" }}>
+           Welcome Finance Manager
+         </h1>
+         <Container>
+           <Row>
+             <Card>
+               <Card.Body>
+                 <Col>
+                   <Table striped bordered hover>
+                     <thead>
+                       <tr>
+                         <th>Sr. No.</th>
+                         <th>PoID</th>
+                         <th>prodName</th>
+                         <th>qty</th>
+                         <th>orderValue</th>
+                         <th>status</th>
+                       </tr>
+                     </thead>
+                     <tbody>
+                       {masterTableData.map((order, index) => (
+                         <tr>
+                           <td>{index + 1}</td>
+                           <td>
+                             <Button
+                             style={{ backgroundColor: "transparent",
+                                   border: "none",color:"black",textDecoration:"underline"  }}
+                              //  onClick={() => handleShow(order)}
+                               variant="primary"
+                             >
+                               {order[1]}
+                             </Button>{" "}
+                           </td>
+                           <td>{order[2]}</td>
+                           <td>{formatBigNumber(order[3])}</td>
+                           <td>{formatBigNumber(order[4])}</td>
+                           <td>{order[6]}</td>
+                         </tr>
+                       ))}
+                     </tbody>
+                   </Table>
+                   <Modal className="mt-5" show={show} onHide={handleClose}>
+                     <Modal.Header closeButton>
+                       <Modal.Title>
+                         Update Bar-code, Batch Number,Master Label
+                       </Modal.Title>
+                     </Modal.Header>
+                     <Modal.Body>
+                       <Form>
+                         <Form.Group
+                           className="mb-3"
+                           controlId="barCode"
+                         >
+                           <Form.Label>Bar-Code</Form.Label>
+                           <Form.Control
+                             type="text"
+                             placeholder=""
+                             onChange={handleChange}
+                           />
+                         </Form.Group>
+                         <Form.Group
+                           className="mb-3"
+                           controlId="batchNumber"
+                         >
+                           <Form.Label>Batch Number</Form.Label>
+                           <Form.Control
+                             type="text"
+                             placeholder=""
+                             onChange={handleChange}
+                           />
+                         </Form.Group>
+                         <Form.Group
+                           className="mb-3"
+                           controlId="masterLabel"
+                         >
+                           <Form.Label>Master Label</Form.Label>
+                           <Form.Control
+                             type="text"
+                             placeholder=""
+                             onChange={handleChange}
+                           />
+                         </Form.Group>
+                       </Form>
+                     </Modal.Body>
+                     <Modal.Footer>
+                       <Button variant="secondary" onClick={handleClose}>
+                         Close
+                       </Button>
+                       <Button variant="primary" onClick={handleSave}>
+                         Save Changes
+                       </Button>
+                     </Modal.Footer>
+                   </Modal>
+                 </Col>
+               </Card.Body>
+             </Card>
+           </Row>
+         </Container>
+       </div>
+     </Navbar>
+   );
+ } else {
+   return (
+     <Navbar pageTitle={"Delivery Hub"} navItems={navItem}>
+       <div>
+         <Container>
+           <Row>
+             <Card>
+               <Card.Body>
+                 <h1>You don't have permission</h1>
+               </Card.Body>
+             </Card>
+           </Row>
+         </Container>
+       </div>
+     </Navbar>
+   );
+ }
+};
